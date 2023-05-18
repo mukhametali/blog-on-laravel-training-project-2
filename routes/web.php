6d9990bfd5eaf0d800b71691b48ab433018594e9
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-18 19:15:49 +0600

    route with slug. previous and next post. Carousel

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
+Route::get('/posts/{slug}','HomeController@show')->name('post.show');
+
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::get('/','DashboardController@index');
